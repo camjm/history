33944e7d53666cdde5c66dd05e7e13b51337982b
Cam <cam@mackay.net.nz>
2017-01-28 13:50:26 +1300

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+/*
+ * History Script
+ */
+
 function supportsHistoryApi() {
   return !!(window.history && history.pushState);
 }
 
 function swap(href) {
   var req = new XMLHttpRequest();
-  req.open("GET", "/gallery/" + href.split("/").pop(), false);
+  // contruct URL to hidden partial page
+  var url = "/gallery/" + href.split("/").pop()
+  req.open("GET", url, false);
   req.send(null);
   if (req.status == 200) {
+    // insert the new content into the current page
     document.getElementById("gallery").innerHTML = req.responseText;
+    // have to re-add the event handlers to the new content
     setup();
     return true;
   }
@@ -16,14 +24,19 @@ function swap(href) {
 
 function addHandler(link) {
   link.addEventListener("click", function(e) {
+    // the URL to appear in the location bar
+    var url = link.href;
     if (swap(link.href)) {
-      history.pushState(null, null, link.href);
+      // the swap is successfull, so manually update the URL without refreshing the page
+      // note: the first argument is 'state', which will be passed to the 'popstate' event handler
+      history.pushState(null, null, url);
       e.preventDefault();
     }
   }, true);
 }
 
 function setup() {
+  // setup the navigation handers
   addHandler(document.getElementById("previous"));
   addHandler(document.getElementById("next"));
 }
@@ -32,7 +45,9 @@ window.onload = function() {
   if (!supportsHistoryApi()) return;
   setup();
   window.setTimeout(function(){
+    // triggered when the user presses the 'back' button
     window.addEventListener("popstate", function(e){
+      // manually go back to the previous state (location is already updated to previous page)
       swap(location.pathname);
     }, false);
   }, 1);
